5a97705622dd874782fc047f8146197fd287c9bb
oono198279-www <oono198279@gmail.com>
2025-11-14 11:15:05 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'lathe-tool-setup-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './blade.png',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  // HTML はネット優先、それ以外はキャッシュ優先
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req).catch(() => caches.match('./index.html'))
+    );
+  } else {
+    event.respondWith(
+      caches.match(req).then(res => res || fetch(req))
+    );
+  }
+});
